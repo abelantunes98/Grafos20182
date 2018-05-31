10fbd25c3f2a4a22517d397097ba7d5486e4b350
abelantunes98 <abel.neto@ccc.ufcg.edu.br>
2018-05-30 21:55:38 -0300

    1

--- Graph Teory - Practical Exercise 2 - Question 2/src/grafos2/DefaultVertex.java
+++ Graph Teory - Practical Exercise 2 - Question 2/src/grafos2/DefaultVertex.java
@@ -5,38 +5,78 @@ import java.util.Map;
 
 import org.jgrapht.io.Attribute;
 
+/**
+ *This class will construct a standard vertex.
+ *
+ * @author Abel Antunes
+ * @author José Guilherme
+ * @author Mariana Nascimento
+ * @author Siuanny Barbosa
+ */
+
 public class DefaultVertex implements Serializable {
 
 	private static final long serialVersionUID = 1;
 
 	private Object id;
 	private Map<String, Attribute> att;
-
+	
+	/**
+	 * This is the class constructor, where the variables id and att will be assigned.
+	 * 
+	 * @param id
+	 * @param att
+	 */
 	public DefaultVertex(Object id, Map<String, Attribute> att) {
 		this.id = id;
 		this.att = att;
 	}
-
+	
+	/**
+	 * Method that returns the id.
+	 */
 	public Object getId() {
 		return id;
 	}
-
+	
+	/**
+	 * Method that returns the att toString.
+	 */
 	public String getAtt() {
 		return att.toString();
 	}
-
+	
+	/**
+	 * Method that defines a new value for att.
+	 */
 	public void setAtt(Map<String, Attribute> att) {
 		this.att = att;
 	}
-
+	
+	/**
+	 * Method that returns the att label.
+	 */
 	public String getLabel() {
 		return (att.get("label")).toString();
 	}
-
+	
+	/**
+	 * Output class text.
+	 */
 	public String toString() {
 		return (att.get("label")).toString();
 	}
-
+	
+	/**
+	 * A method that compares if an exterior vertex is equal to the vertex in question.
+	 * @param v
+	 * 
+	 * Auxiliary Variable
+	 * 
+	 * @return
+	 * Returns True if they are equal and False if they are different.
+	 * 
+	 */
 	public boolean equals(DefaultVertex v) {
 		return this.id.equals(v.id);
 	}
--- Graph Teory - Practical Exercise 2 - Question 2/src/grafos2/FindPath.java
+++ Graph Teory - Practical Exercise 2 - Question 2/src/grafos2/FindPath.java
@@ -7,14 +7,34 @@ import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 
+/**
+ * This class finds the closest path between two vertices.
+ * 
+ * @author Abel Antunes
+ * @author José Guilherme
+ * @author Mariana Nascimento
+ * @author Siuanny Barbosa
+ */
 public class FindPath {
-
+	
+	/**
+	 * This method uses two vertices and tells the smallest path between them.
+	 * @param graphGml - Used graph
+	 * @param sink - Vertex Dice
+	 * @return - Smallest path between vertices
+	 */
 	public static GraphPath<Object, RelationshipEdge> searchShortestPath(Graph<Object, RelationshipEdge> graphGml, Object sink) {
 		DefaultVertex C = findMachine(graphGml, "C");
 		DefaultVertex M = findMachine(graphGml, sink);
 		return DijkstraShortestPath.findPathBetween(graphGml, C, M);
 	}
-
+	
+	/**
+	 * This method looks for the vertexs information.
+	 * @param graphGml - Used graph
+	 * @param sink - Vertex Dice
+	 * @return - The vertexs information
+	 */
 	private static DefaultVertex findMachine(Graph<Object, RelationshipEdge> graphGml, Object sink) {
 		Set<Object> vertex = graphGml.vertexSet();
 		Iterator<Object> iter = vertex.iterator();
